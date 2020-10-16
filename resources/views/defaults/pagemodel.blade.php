b79e2924adc28f025a78cbf417ba339f2aa460a3
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-16 15:05:24 -0300

    🎨 New default page model template

---
+++ resources/views/defaults/pagemodel.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main')
+
+@section('title', ''.config('app.separator'))
+
+@section('content')
+<div class="row content">
+    <div class="col">
+
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+<script type="text/javascript">
+    // Add custom scripts here
+</script>
+@endpush
+
+@push('styles')
+<style type="text/css">
+    /* Add custom styles here */
+</style>
+@endpush
