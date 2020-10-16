5dac48787e650668ba24a3b9112004ff5ed07da5
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-16 16:59:51 -0300

    🏗 Insurances init

---
+++ app/Models/Insurances/Insurance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Insurances;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Insurance extends Model
+{
+    use HasFactory;
+}
