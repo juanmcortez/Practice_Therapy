f052b494ae2ff90601c606cd2103ba094b5ca2ad
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-16 14:01:20 -0300

    💄 UI start

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js('resources/assets/js/practice_therapy.js', 'public/js')
+    .sass('resources/assets/sass/practice_therapy.scss', 'public/js');
