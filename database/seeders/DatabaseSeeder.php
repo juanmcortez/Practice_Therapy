45c4760b942b095c95fc35e8d4437c8a08a999cf
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-16 18:04:16 -0300

    🗃 Initial fake creation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Doctors\Doctor;
+use App\Models\Insurances\Insurance;
+use App\Models\Patients\Patient;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\Users\User::factory(10)->create();
+        Patient::factory(12500)->create();
+        Doctor::factory(250)->create();
+        Insurance::factory(75)->create();
     }
 }
