708415159f30b6ad3ee1336c1edb10fd89625da8
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-20 14:34:48 -0300

    🗃 Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Demographics\Address;
 use App\Models\Doctors\Doctor;
 use App\Models\Insurances\Insurance;
+use App\Models\Patients\AddressPatient;
 use App\Models\Patients\InsurancePatient;
 use App\Models\Patients\Patient;
 use Illuminate\Database\Seeder;
@@ -20,19 +22,37 @@ class DatabaseSeeder extends Seeder
         $maxNumberOfIns = 3;
         $TotalNumOfInsr = 75;
         $TotalNumOfDocs = 250;
-        $TotalNumOfPats = 12500;
+        $TotalNumOfPats = 100;
 
         // Create insurances companies
+        $this->command->info('Creating insurances');
         Insurance::factory()->count($TotalNumOfInsr)->create();
 
         // Create Doctors list
+        $this->command->info('Creating doctors');
         Doctor::factory()->count($TotalNumOfDocs)->create();
 
         // Create Patients with the insurances relations
+        $this->command->info('Creating patients');
         Patient::factory()->count($TotalNumOfPats)->create();
 
+        $this->command->info('Creating patient addresses');
+        Address::factory()->count($TotalNumOfPats)->create();
+
         // Pivot Insurances / Patients
+        $this->command->info('Linking');
         foreach (Patient::all() as $patient) {
+            // Add Address
+            AddressPatient::factory()
+                ->count(1)
+                ->create(
+                    [
+                        'patient_id' => $patient->id,
+                        'address_id' => $patient->id,
+                    ]
+                );
+
+            // Add insurances
             $randIns = random_int(1, 3);
             $oldIns = array();
             for ($i = 1; $i <= $randIns; $i++) {
