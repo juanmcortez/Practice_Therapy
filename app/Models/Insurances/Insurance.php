c633cb46d02fc7bbc850fde47101ba009235c935
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-20 16:23:54 -0300

    🗃 🚧 Insurance address

--- app/Models/Insurances/Insurance.php
+++ app/Models/Insurances/Insurance.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Insurances;
 
+use App\Models\Demographics\Address;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,4 +10,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Insurance extends Model
 {
     use HasFactory, SoftDeletes;
+
+    // This relationship uses a pivot table
+    public function addresses()
+    {
+        return $this->belongsToMany(Address::class);
+    }
 }
