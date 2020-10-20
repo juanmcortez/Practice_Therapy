59dfa30b34b008c5ef2be465956863316ac4fc07
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-20 17:58:33 -0300

    🚧 Customizing model properties

--- app/Models/Demographics/Address.php
+++ app/Models/Demographics/Address.php
@@ -9,4 +9,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Address extends Model
 {
     use HasFactory, SoftDeletes;
+
+    /**
+     * Elements hidden from json request
+     * @var array
+     */
+    protected $hidden = [
+        'id', 'pivot', 'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    /**
+     * Elements that are mass asignable.
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'main', 'extended', 'city',
+        'state', 'zip', 'country', 'pivot'
+    ];
 }
