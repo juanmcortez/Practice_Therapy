5ea4f367ae9a3fbe8ddca7b9a90c20bafda2056d
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-20 12:09:28 -0300

    🚧 Visualize relationship

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,49 +8,63 @@
 
 @section('content')
 @php
-foreach ($data as $item) { dd($item->insurances); }
+//foreach ($data as $item) { dd($item); }
 @endphp
-<!--
+
 <div class="row content">
     <div class="col text-center">
         <h1>{{ config('app.name') }}</h1>
     </div>
 </div>
+
 @if(isset($data))
 <div class="row content">
     <div class="col text-center">
         {{ $data->links() }}
     </div>
 </div>
-@endif
+
+@foreach ($data as $item)
 <div class="row content">
     <div class="col text-center">
-        @if(isset($data))
-        @foreach ($data as $item)
-        {{ $item->id.' '.$item->externalID }}
-        <br />
+        {{ $item->id }}
+    </div>
+    <div class="col text-center text-muted">
+        {{ $item->externalID }}
+    </div>
+    <div class="col text-center">
         @if(isset($item->name))
         {{ $item->name }}
         @else
         {{ $item->last_name.', '.$item->first_name.' '.$item->middle_name }}
         @endif
-        @if(isset($item->specialty))
-        <br />
-        {{ $item->specialty }}
-        @endif
-        <hr />
-        @endforeach
-        @endif
     </div>
 </div>
-@if(isset($data))
+
+@foreach ($item->insurances as $key => $insurance)
+<div class="row content">
+    <div class="col text-left">
+        Ins {{ $key+1 }}: <span class="text-muted">{{ $insurance->name }}</span>
+    </div>
+</div>
+@endforeach
+
+@if(isset($item->specialty))
+<div class="row content">
+    {{ $item->specialty }}
+</div>
+@endif
+
+<hr />
+@endforeach
+
 <div class="row content">
     <div class="col text-center">
         {{ $data->links() }}
     </div>
 </div>
 @endif
--->
+
 @endsection
 
 @push('scripts')
