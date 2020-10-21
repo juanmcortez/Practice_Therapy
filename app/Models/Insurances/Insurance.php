cb98311eb37cd44cf7cff4c3b16143e44a123322
Juan M. Cortéz <juanm.cortez@gmail.com>
2020-10-21 12:00:22 -0300

    🗃 🚧 Insurance phone relationship

--- app/Models/Insurances/Insurance.php
+++ app/Models/Insurances/Insurance.php
@@ -3,6 +3,7 @@
 namespace App\Models\Insurances;
 
 use App\Models\Demographics\Address;
+use App\Models\Demographics\Phone;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
---
+++ app/Models/Insurances/InsurancePhone.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Insurances;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class InsurancePhone extends Pivot
+{
+    use HasFactory;
+}
